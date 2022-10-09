7e69f183d167f086103e89ba78d55eeb73b0d91f
Stanislav <stanislav.ryzovas@jkm.lt>
2022-10-09 13:09:57 +0300

    Added tasks: 7,8,9

---
+++ python.py
@@ -0,0 +1,24 @@
+# #įvedimas iš klaviatūros funkcija metai, mėnuo, diena
+
+# metai = input("Įveskite šios dienos datos metus ")
+# mėnuo = input("Įveskite šios dienos datos mėnesį ")
+# diena = input("Įveskite šios dienos datos dieną ")
+# print("Šiandien yra", metai, mėnuo, diena)
+# coverted_metai = int(metai)
+# coverted_mėnuo = int(mėnuo)
+# coverted_diena = int(diena)
+# print("Datos skaičių suma = ", coverted_metai + coverted_mėnuo + coverted_diena)
+
+
+# print("Laba Diena!")
+# diena = input("Įveskite šios dienos datos dieną :")
+# coverted_diena = int(diena)
+# print("Nuo metų pradžios praėjo", 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30, "diena")
+
+
+# print("Laba diena!")
+# mėnuo = input("Įveskite šios dienos datos mėnesį : ")
+# diena = input("Įveskite šios dienos datos dieną : ")
+# coverted_mėnuo = int(mėnuo)
+# coverted_diena = int(diena)
+# print("Nuo metų pradžios praėjo", (coverted_mėnuo - 1) * 30 + coverted_diena)
